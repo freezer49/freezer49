68d2925865fd96a1b4a2b9e46e040dee350f4f05
Chloe Verglas <chloe.verglas@gmail.com>
2025-11-17 15:56:07 +0100

    typescript

---
+++ JavaScript/typeScript/exercices/ex1/script.ts
@@ -0,0 +1,27 @@
+// 1. Définis l'interface User en regardant les données
+export interface User {
+  name: string;
+  age: number;
+  occupation: string;
+}
+
+// 2. Type le tableau avec User[]
+
+export const users: User[] = [
+  {
+    name: "Max Mustermann",
+    age: 25,
+    occupation: "Chimney sweep",
+  },
+  {
+    name: "Kate Müller",
+    age: 23,
+    occupation: "Astronaut",
+  },
+];
+
+export function logPerson(user: User) {
+  console.log(` - ${user.name}, ${user.age}`);
+}
+console.log("Users:");
+users.forEach(logPerson);
